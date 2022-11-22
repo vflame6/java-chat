a8c789271102425e90eabd16f8d35343dc23ac94
Maksim <80809363+vflame6@users.noreply.github.com>
2022-11-22 18:58:51 +0300

    Добавил переменные состояния isAuthenticated и isAdmin. Реализовал логин со стороны сервера. Для остальных функций сделал заглушки в switch/case и в методах.
    Переменные состояния в каждом подключении изначально false, затем, при логине меняют состояние и таким образом будет выстроена проверка доступных функций для каждого подключения.
    В switch/case ищется совпадение для команды, если не находит отдаёт INVALID_COMMAND. Если найдёт команду, то нужно распаковать пришедшие данные и отправить их в специальный метод для каждого случая.
    Пока что везде заглушки, кроме login.

--- Server/chatServer/ChatThread.java
+++ Server/chatServer/ChatThread.java
@@ -1,12 +1,13 @@
 package chatServer;
 
 import java.io.*;
+import java.util.Objects;
 import javax.net.ssl.*;
-
-import static chatServer.Cookies.getCookie;
 
 public class ChatThread extends Thread {
     private SSLSocket sslSocket;
+    private boolean isAuthenticated = false; // Authenticated state
+    private boolean isAdmin = false; // Admin state
 
     public ChatThread(SSLSocket sslSocket) {
         this.sslSocket = sslSocket;
@@ -31,66 +32,119 @@ public class ChatThread extends Thread {
             try {
                 line = inp.readLine();
                 System.out.println(line);
-                String[] command = line.split(" ");
+                String[] command = line.split(";");
+                String output = "";
+
                 switch (command[0]) {
+                    // login(String username, String password)
                     case ("LOGIN"):
-                        String usernameLogin = command[1];
-                        String passwordLogin = command[2];
-                        if (usernameLogin.equals("admin")) {
-                            out.write(("OK " + usernameLogin + " 9999999999" + " 1 "
-                                    + getCookie() + '\n').getBytes());
-                        } else {
-                            out.write(("INVALID_CREDENTIALS" + '\n').getBytes());
-                        }
+                        String[] credentials = command[1].split(" ");
+                        String username = credentials[1];
+                        String password = credentials[2];
+
+                        output = login(username, password);
                         break;
+                    // loginCookie(String value)
+                    case ("LOGIN_COOKIE"):
+                        break;
+                    // register(String username, String telephone, String password)
                     case ("REGISTER"):
-                        String usernameRegistration = command[1];
-                        String telephone = command[2];
-                        String password = command[3];
-                        if (usernameRegistration.equals("admin")) {
-                            out.write(("OK" + '\n').getBytes());
-                            // add to bd
-                        } else {
-                            out.write(("INVALID_USERNAME OR INCORRECT_TELEPHONE " +
-                                    +'\n').getBytes());
-                        }
+                        break;
+                    // getMessages()
+                    case ("GET_MESSAGES"):
+                        break;
+                    // sendMessage(String from, String content)
+                    case ("SEND_MESSAGE"):
+                        break;
+                    // deleteMessage(int id)
+                    case ("DELETE_MESSAGE"):
+                        break;
+                    // Command not exists:
+                    default:
+                        output = "INVALID_COMMAND";
                         break;
                 }
-
-                // LOGIN;USERNAME PASSWORD
-                // OK;USERNAME TELEPHONE IS_ADMIN SESSION_COOKIE
-                // INVALID_CREDENTIALS;
-
-                // REGISTER;USERNAME TELEPHONE PASSWORD
-                // OK;USERNAME TELEPHONE IS_ADMIN
-                // INVALID_USERNAME;
-                // INCORRECT_TELEPHONE;
-
-                // LOGIN_COOKIE;VALUE
-                // OK; USERNAME TELEPHONE IS_ADMIN SESSION_COOKIE
-                // INCORRECT_COOKIE;
-
-                // AUTHENTICATED ONLY FUNCTIONS:
-                // without auth returns AUTHENTICATION_REQUIRED;
-
-                // GET_MESSAGES;
-                // OK;[MESSAGE1, MESSAGE2, ...]
-
-                // SEND_MESSAGE;MESSAGE
-                // OK;MESSAGE_ID
-
-                // DELETE_MESSAGE;ID
-                // OK;
-                // NO_ADMIN_RIGHTS;
-                // NO_SUCH_MESSAGE;
-
-                // If command not exists
-                // INVALID_COMMAND
-
+                output += "\n";
+                out.write(output.getBytes());
             } catch (IOException e) {
-                e.printStackTrace();
+                System.out.println("Connection closed from: " +
+                        sslSocket.getRemoteSocketAddress());
                 return;
             }
         }
+    }
+
+    // Input:
+    // LOGIN;USERNAME PASSWORD
+    // Returns:
+    // OK;USERNAME TELEPHONE IS_ADMIN SESSION_COOKIE
+    // INVALID_CREDENTIALS;
+    private String login(String username, String password) {
+        String passwordHash = PasswordHash.getPasswordHash(password);
+        User user = DBConnect.getUser(username, passwordHash);
+
+        if (Objects.isNull(user)) {
+            return "INVALID_CREDENTIALS;";
+        }
+        isAuthenticated = true;
+        if (user.getIsAdmin() == 1) {
+            isAdmin = true;
+        }
+
+        String sessionCookie = Cookies.getCookie();
+        DBConnect.createSession(user.getId(), sessionCookie);
+        return "OK;" + user.getUsername() + " " +
+                user.getTelephone() + " " +
+                sessionCookie;
+    }
+
+    // Input:
+    // LOGIN_COOKIE;VALUE
+    // Output:
+    // OK; USERNAME TELEPHONE IS_ADMIN SESSION_COOKIE
+    // INCORRECT_COOKIE;
+    private String loginCookie(String value) {
+        return null;
+    }
+
+    // Input:
+    // REGISTER;USERNAME TELEPHONE PASSWORD
+    // Output:
+    // OK;USERNAME TELEPHONE IS_ADMIN
+    // INVALID_USERNAME;
+    // INCORRECT_TELEPHONE;
+    private String register(String username, String telephone, String password) {
+        return null;
+    }
+
+    // AUTHENTICATED only functions:
+    // without AUTHENTICATED returns AUTHENTICATION_REQUIRED;
+
+    // Input:
+    // GET_MESSAGES;
+    // Output:
+    // OK;[MESSAGE1, MESSAGE2, ...]
+    private String getMessages() {
+        return null;
+    }
+
+    // Input:
+    // SEND_MESSAGE;MESSAGE
+    // Output:
+    // OK;MESSAGE_ID
+    private String sendMessage(String from, String content) {
+        return null;
+    }
+
+    // AUTHENTICATED and ADMIN only functions:
+
+    // Input:
+    // DELETE_MESSAGE;ID
+    // Output:
+    // OK;
+    // NO_ADMIN_RIGHTS;
+    // NO_SUCH_MESSAGE;
+    private String deleteMessage(int id) {
+        return null;
     }
 }
