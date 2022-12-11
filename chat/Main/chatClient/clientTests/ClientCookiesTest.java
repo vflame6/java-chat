3b4bab0ae88e8d98c651c316cbe288859d5bc56b
acediacode <sasha.kabanov2003@mail.ru>
2022-12-11 20:52:29 +0300

    Добавил junit test
    Чтобы они корректно работали нужно разархивировать junit.zip и перенести содержимое в папку lib

---
+++ chat/Main/chatClient/clientTests/ClientCookiesTest.java
@@ -0,0 +1,65 @@
+package chat.Main.chatClient.clientTests;
+
+import chat.Main.chatClient.ClientCookies;
+import org.junit.jupiter.api.Test;
+
+import java.io.FileNotFoundException;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ClientCookiesTest {
+
+    ClientCookies test = new ClientCookies();
+
+    @Test
+    void getCookieTest123() {
+        String cookie = "123";
+        test.createCookie(cookie);
+        assertEquals("123", test.getCookie());
+    }
+
+@Test
+    void getCookieTest567() {
+        String cookie = "567";
+        test.createCookie(cookie);
+        assertEquals("567", test.getCookie());
+    }
+
+    @Test
+    void getCookieTestString() {
+        String cookie = "String";
+        test.createCookie(cookie);
+        assertEquals("String", test.getCookie());
+    }
+
+    @Test
+    void getCookieTest() {
+        String cookie = "String\n123";
+        test.createCookie(cookie);
+        assertEquals("String\n123", test.getCookie());
+    }
+
+    @Test
+    void deleteCookieTest() {
+        String cookie = "123";
+        test.createCookie(cookie);
+        test.deleteCookie();
+        assertThrows(RuntimeException.class,() ->{
+            test.getCookie();
+        });
+    }
+
+    @Test
+    void cookieExistsTest() {
+        test.createCookie("test");
+        assertTrue(test.isCookieExists());
+    }
+
+    @Test
+    void cookieNotExistsTest() {
+        test.deleteCookie();
+        assertFalse(test.isCookieExists());
+    }
+
+
+}
---
+++ chat/Main/chatClient/clientTests/ClientFunctionalTest.java
@@ -0,0 +1,63 @@
+package chat.Main.chatClient.clientTests;
+
+import chat.Main.InvalidTelephoneException;
+import chat.Main.chatClient.AuthenticationRequiredException;
+import chat.Main.chatClient.ClientFunctional;
+import chat.Main.chatClient.NoAdminRightsException;
+import org.junit.jupiter.api.Test;
+
+import java.io.File;
+import java.io.FileReader;
+import java.net.UnknownHostException;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ClientFunctionalTest {
+    ClientFunctional test;
+    {
+        try {
+            test = new ClientFunctional("localhost");
+        } catch (UnknownHostException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    @Test
+    void pingTest() {
+        assertTrue(test.ping());
+    }
+
+    @Test
+    void getMessagesTest() {
+        assertThrows(AuthenticationRequiredException.class,()->{
+            test.getMessages();
+        });
+    }
+    @Test
+    void loginTest() {
+        assertFalse(test.login("testsuser","testspassword"));
+    }
+    @Test
+    void phoneNumberTest() {
+        assertThrows(InvalidTelephoneException.class,()->{test.register("testuser","testpassword","+79");});
+    }
+    @Test
+    void privateKeyExistsTest(){
+        File keys=new File("encryption/private.key");
+        assertTrue(keys.exists());
+    }
+    @Test
+    void deleteMessageTestWithoutLogin() {
+        assertThrows(AuthenticationRequiredException.class,()->{
+            test.deleteMessage(1);
+        });
+    }
+        @Test
+        void deleteMessageTestWithLogin() {
+            assertThrows(NoAdminRightsException.class,()->{
+                test.login("testuser","testpassword");
+                test.deleteMessage(1);
+            });
+        }
+    }
+
+
