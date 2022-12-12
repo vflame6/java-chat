ca1d59d81d64cbaa0a046adf7ff6a1f18c0e260e
jnova6 <pannova.ja@gmail.com>
2022-12-12 15:16:44 +0300

    Работа с GUI. Сделала окно чата. Настроила работу с сообщениями. Для того чтобы получить новые сообщения надо нажать кнопку обновить.

---
+++ chat/Main/chatClient/ChatChatController.java
@@ -0,0 +1,85 @@
+package chat.Main.chatClient;
+import chat.Main.Message;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+
+import java.sql.Timestamp;
+import java.util.List;
+
+public class ChatChatController {
+    ClientFunctional clientFunctional = ClientHolder.getInstance().getClient();
+    @FXML
+    private AnchorPane chat;
+    @FXML
+    private Button logOutButton;
+    @FXML
+    private Button updateButton;
+    @FXML
+    private Label chatName;
+    @FXML
+    private Label chatName1;
+
+    @FXML
+    private TextField enterMessage;
+    @FXML
+    private Button sendButton;
+
+    private int minx = 5;
+    private int miny = 15;
+    private int distance = 0;
+    private Timestamp time;
+    private List<Message> messageList;
+
+
+    @FXML
+    void initialize() {
+        if (clientFunctional.getMessages()) {
+            messageList = clientFunctional.messageList;
+            time = messageList.get(messageList.size() - 1).getDate();
+            for (Message message : messageList) {
+                displayMessage(message);
+                distance += 5;
+                }
+            miny = 15;
+            distance = 0;
+            }
+
+        updateButton.setOnAction((event) -> {
+            clientFunctional.getLastMessageTimestamp();
+            if (clientFunctional.lastMessageTimestamp.compareTo(time) != 0) {
+                if (clientFunctional.getMessages()) {
+                    chat.getChildren().clear();
+                    messageList = clientFunctional.messageList;
+                    time = messageList.get(messageList.size() - 1).getDate();
+                    for (Message message : messageList) {
+                        displayMessage(message);
+                        distance += 5;
+                    }
+                }
+                miny = 15;
+                distance = 0;
+            }
+        });
+
+            sendButton.setOnAction((event) -> {
+                System.out.println("1");
+                String message = enterMessage.getText();
+                clientFunctional.sendMessage(message);
+            });
+        }
+        public void displayMessage(Message message){
+            int rows = message.getContent().length() / 44 + 1;
+            Label lbl = new Label();
+            lbl.setMinHeight(35 * rows);
+            lbl.setMinWidth(420);
+            lbl.setLayoutX(minx);
+            lbl.setLayoutY(miny + distance);
+            lbl.setText(" " + message.getContent());
+            miny += 35 * rows;
+            chat.getChildren().add(lbl);
+        }
+    }
+
